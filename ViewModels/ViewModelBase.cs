de8938eecce14c4937c8a7c042c92758a4f49e6a
Mike <dreamer94mv@gmail.com>
2022-07-19 17:31:42 +0500

    Init commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace VEGASTAR.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
