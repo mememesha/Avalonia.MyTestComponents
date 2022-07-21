325d071a1a54da4f3628114029108d6c4df9c447
Mike <dreamer94mv@gmail.com>
2022-07-21 11:03:32 +0500

    Add app fonts

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,6 @@ namespace VEGASTAR.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private bool state;
-        private bool IsOpen
-        {
-            get => state;
-            set => this.RaisePropertyChanged(nameof(IsOpen));
-        }
+       
     }
 }
