be01b501518e478ed9218bb3e6dcdd1d685e9538
mememesha <dreamer94mv@gmail.com>
2022-08-01 21:59:11 +0500

    fix 2

--- WorkSolution/UI/ViewModels/SubRouteUserControlViewModel.cs
+++ WorkSolution/UI/ViewModels/SubRouteUserControlViewModel.cs
@@ -9,42 +9,11 @@ namespace VEGASTAR.ViewModels;
 
 public class SubRouteUserControlViewModel : ReactiveObject, IScreen, IRoutableViewModel
 {
-    private AppBarMenuItem _selectedMenuItem;
-
     public SubRouteUserControlViewModel(IScreen screen)
     {
         HostScreen = screen;
-
-        BarMenuItems = new ObservableCollection<AppBarMenuItem>
-        {
-            new()
-            {
-                Caption = "Загрузка", Icon = "fa-solid fa-bars-progress",
-                RouteViewModel = typeof(AdisLocalStorageViewModel)
-            },
-            new()
-            {
-                Caption = "Карты", Icon = "fa-solid fa-file-arrow-up",
-                RouteViewModel = typeof(AdisLocalStorageViewModel)
-            }
-        };
-
-        _selectedMenuItem = _selectedMenuItem ?? BarMenuItems.First();
     }
-
-    public AppBarMenuItem SelectedMenuItem
-    {
-        get => _selectedMenuItem;
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _selectedMenuItem, value);
-            if (_selectedMenuItem.RouteViewModel == typeof(AdisLocalStorageViewModel))
-                Router.NavigateAndReset.Execute(new AdisLocalStorageViewModel(this));
-        }
-    }
-
-    public ObservableCollection<AppBarMenuItem> BarMenuItems { get; set; }
-
+    
     public IScreen HostScreen { get; }
 
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
